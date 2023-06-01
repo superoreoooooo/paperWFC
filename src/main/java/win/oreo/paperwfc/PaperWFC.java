dede0012fb6e2eb7e3f1d165c0f58258e87dabc2
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-06-01 21:55:38 +0900

    hello World

---
+++ src/main/java/win/oreo/paperwfc/PaperWFC.java
@@ -0,0 +1,14 @@
+package win.oreo.paperwfc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PaperWFC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
